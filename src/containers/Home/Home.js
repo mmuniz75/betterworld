7fffbe6821971046ed7a145b9b3fcb6af1ff0dd2
marcelo <muniz_marcelo@hotmail.com>
2019-01-17 16:11:44 -0200

    correacao de imagem e barra fixa de menu

--- src/containers/Home/Home.js
+++ src/containers/Home/Home.js
@@ -78,7 +78,7 @@ class Home extends Component {
                 <div className="col-twelve">
                    <section>  
                         <div className="content-media">						
-		                    <img src="/images/hands-around-world-backgrounds-wallpapers.jpg" alt="" style={{width:1000, height:400}} />						  
+		                    <img src="/images/hands-around-world-backgrounds-wallpapers.jpg" alt=""  />						  
                         </div>
 
                         <div className="primary-content">
